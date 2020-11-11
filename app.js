24a30bc45d87766901364ffcfb9cca7e95822689
jallend1 <jallend1@gmail.com>
2020-11-11 13:37:21 -0800

    Begins scaffolding out rudimentary page for centralized location for all quotes

--- app.js
+++ app.js
@@ -4,18 +4,33 @@ import { episodeArray } from "./episodes.js";
 
 const refreshButton = document.querySelector("#refresh");
 
+const renderAllEpisodes = () => {
+  const displayArea = document.querySelector('main');
+  episodeArray.forEach(episode => {
+    displayArea.innerHTML += `
+    <div class="episode-quote">
+      <div>
+        ${episode.season}x${episode.episodeNumber} - ${episode.title}
+      </div>
+      <div>
+        ${episode.quote}
+      </div>
+    </div>`
+  })
+}
+
 const renderSingleEpisode = () => {
+  refreshButton.addEventListener("click", renderSingleEpisode);
+
   const display = document.querySelector("main");
   const episodeDiv = document.querySelector("#episode");
   const titleDiv = document.querySelector("#title");
-  const listLength = episodeArray.length;
-  const randomEpisode = Math.floor(Math.random() * listLength) + 1;
+  const randomEpisode = Math.floor(Math.random() * episodeArray.length) + 1;
 
   display.innerHTML = `<h1>${episodeArray[randomEpisode].quote}.</h1>`;
   titleDiv.innerHTML = `<div>"${episodeArray[randomEpisode].title}"</div>`;
   episodeDiv.innerHTML = `<div>Season ${episodeArray[randomEpisode].season} Episode ${episodeArray[randomEpisode].episodeNumber}`;
 };
 
-renderSingleEpisode();
-
-refreshButton.addEventListener("click", renderSingleEpisode);
+// IF the refresh button has a value, it's on the quote page so show a single quote. If not, you're on the all episodes page
+refreshButton ? renderSingleEpisode() : renderAllEpisodes();
