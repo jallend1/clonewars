3da315503e4b57a9d3d59d990542f4605ce7ec4b
jallend1 <jallend1@gmail.com>
2020-11-16 17:33:21 -0800

    Moves episode details of quote to just below quote and out of footer. Enlarges font of the main quote itself. Refactors episode number display for readability

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const renderSingleEpisode = () => {
   refreshButton.addEventListener("click", renderSingleEpisode);
 
   const display = document.querySelector("main");
-  const episodeDiv = document.querySelector("#episode");
-  const titleDiv = document.querySelector("#title");
+  const details = document.querySelector('.details');
   const randomEpisode = Math.floor(Math.random() * episodeArray.length) + 1;
+  // If less than 9, adds a preceeding 0
+  const formattedEpisodeNum = episodeArray[randomEpisode].episodeNumber > 9 ? episodeArray[randomEpisode].episodeNumber : `0${episodeArray[randomEpisode].episodeNumber}`;
 
   display.innerHTML = `<h1>${episodeArray[randomEpisode].quote}.</h1>`;
-  titleDiv.innerHTML = `<div>"${episodeArray[randomEpisode].title}"</div>`;
-  episodeDiv.innerHTML = `<div>Season ${episodeArray[randomEpisode].season} Episode ${episodeArray[randomEpisode].episodeNumber}`;
+  details.innerHTML = `-- <span>${episodeArray[randomEpisode].title} (0${episodeArray[randomEpisode].season} x ${formattedEpisodeNum})`;
 };
 
 // IF the refresh button has a value, it's on the quote page so show a single quote. If not, you're on the all episodes page
